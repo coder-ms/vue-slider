b4c45c2d99c0fa485c0a6c8bd8fb7ff84af3cef6
coder-ms <marco.sbrascia@gmail.com>
2022-10-27 22:24:03 +0300

    finito

--- js/script.js
+++ js/script.js
@@ -9,95 +9,71 @@ Bonus:
 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
 */
 
-
-
-
-
-
-/*
-const slides = [
-{
-    image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morales',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, 
-    {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, 
-    {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, 
-    {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, 
-    {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-];
-
-console.log(slides);
-*/
-
-
 // Create app
-console.log(Vue);
 const { createApp } = Vue;
 
-
 createApp({
-    data(){
-        return {
-            activeIndex:0,
-            videoGames:{
-                title1: 'Spiderman Miles Morales',
-                title2: 'Ratchet & Clank: Rift Apart',
-                title3: 'Fortnite',
-                title4: 'Stray',
-                title5: "Marvel's Avengers",
-                images: [
-                    'img/01.webp',
-                    'img/02.webp',
-                    'img/03.webp',
-                    'img/04.webp',
-                    'img/05.webp'
-                ],
-            }
-        }
-    },
-    //
-    methods:{
+    data() {
+            return{
+                prevNextGame : 0,
+                videoGames : 
+                [
+                    {
+                        image: 'img/01.webp',
+                        title: 'Marvel\'s Spiderman Miles Morales',
+                        descr: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.'
+                    }, 
+                    {
+                        image: 'img/02.webp',
+                        title: 'Ratchet & Clank: Rift Apart',
+                        descr: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.'
+                    }, 
+                    {
+                        image: 'img/03.webp',
+                        title: 'Fortnite',
+                        descr: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos."
+                    }, 
+                    {
+                        image: 'img/04.webp',
+                        title: 'Stray',
+                        descr: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city'
+                    }, 
+                    {
+                        image: 'img/05.webp',
+                        title: "Marvel's Avengers",
+                        descr: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.'
+                    }
+                ]}
+            },
+    methods :{
         // videogioco precedente
         prevGame(){
-            this.activeIndex--
-            if(this.activeIndex < 0){
-                this.activeIndex = this.videoGames.images.length - 1;
+            this.prevNextGame--;
+            if(this.prevNextGame < 0 ){
+                this.prevNextGame = this.videoGames.length -1;
             }
         },
         // videogioco successivo
         nextGame(){
-            this.activeIndex++
-            if(this.activeIndex > this.videoGames.images.length - 1){
-                this.activeIndex = 0;
+            this.prevNextGame ++;
+            if(this.prevNextGame > this.videoGames.length -1){
+                this.prevNextGame = 0;
             }
         },
         // scroll con intervallo di 7000ms
-        autoScollGames(){
-            this.autoscroll = setInterval(() =>{
+        autoScrollGame(){
+
+            this.autoscroll = setInterval(()=>{
                 this.nextGame();
-            }, 7000)
+            },7000)
         },
         //stop scroll
-        stopAutoScollGames(){
+        stopScroll(){
             clearInterval(this.autoscroll);
-            this.autoscroll = null;
-        },
+        }
+    },
+    mounted(){
+        this.autoScrollGame();
     }
+
 }).mount('#app');
